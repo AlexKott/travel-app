92520ee6e500c315e4ca1d142e813556d78e8fe3
Alex Kott <tetlokax@gmail.com>
2016-05-04 23:47:01 +0200

    Added countries and country route, changed serializer to use default 'id' (api already adjusted)

--- app/router.js
+++ app/router.js
@@ -9,6 +9,8 @@ Router.map(function() {
   this.route('cities');
   this.route('login');
   this.route('city', {path: '/cities/:city_id'});
+  this.route('countries');
+  this.route('country', {path: 'countries/:country_id'});
 });
 
 export default Router;
